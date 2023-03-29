36a7dfa0d29c634557e715769f52d75497c16d1f
enthony17 <125404126+enthony17@users.noreply.github.com>
2023-03-29 17:18:22 -0300

    porra

--- contato.php
+++ contato.php
@@ -32,7 +32,7 @@ if($_SERVER["REQUEST_METHOD"]==="POST"){
     $sobrenome=$_POST["fsobrenome"];
     $telefone=$_POST["ftelefone"];
     //inserir dados na tabela
-    $sql="INSERT INTO agenda(nome, sobrenome, telefone) VALUES('$nome','$sobrenome','$tele')";
+    $sql="INSERT INTO agenda(nome, sobrenome, telefone) VALUES('$nome','$sobrenome','$telefone')";
     if($conn->query($sql)===TRUE){
         echo"dados inseridos"."<br>";   
     }else{
