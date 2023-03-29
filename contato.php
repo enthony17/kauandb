508a0a2222a3ae548d2d55e4b84762a8a10197e4
enthony17 <125404126+enthony17@users.noreply.github.com>
2023-03-29 16:18:35 -0300

    Update contato.php

--- contato.php
+++ contato.php
@@ -29,10 +29,9 @@ if($_SERVER["REQUEST_METHOD"]==="POST"){
     //coleta dados do formulario
     $nome=$_POST["fnome"];
     $sobrenome=$_POST["fsobrenome"];
-    $sobrenome=$_POST["ftelefone"];
+    $telefone=$_POST["ftelefone"];
     //inserir dados na tabela
-    $sql"INSERT INTO agenda(nome, sobrenome, telefone) VALUES('$nome','$sobrenome','$tele
-    ')";
+    $sql="INSERT INTO agenda(nome, sobrenome, telefone) VALUES('$nome','$sobrenome','$tele')";
     if($conn->query($sql)===TRUE){
         echo"dados inseridos"."<br>";   
     }else{
