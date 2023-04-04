cb9d28a039ed39be0b15db8ef54ac6b45b8c6468
enthony17 <125404126+enthony17@users.noreply.github.com>
2023-04-04 17:03:25 -0300

    ghkhfjk

--- kauan/login.php
+++ kauan/login.php
@@ -75,5 +75,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<a href="login.php">Consultar cadastro</a>
 </body>
 </html>
