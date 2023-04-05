a1806129a8cd73b42bd5f78f6f2ecdd081d973ee
enthony17 <125404126+enthony17@users.noreply.github.com>
2023-04-05 14:58:09 -0300

    r6ewastrydukl.

--- kauan/login.php
+++ kauan/login.php
@@ -74,7 +74,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 </div>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> 
+<form>
 <a href="login.php">Consultar cadastro</a>
+            </form>
 </body>
 </html>
